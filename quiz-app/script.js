376acb2ef5331b39b4f41e52a8f9175434022c1d
shoneriki <ikirenohs@gmail.com>
2020-12-22 10:48:24 +0900

    quiz app created

---
+++ quiz-app/script.js
@@ -0,0 +1,23 @@
+// object in javascript, array of quizzes included with question, choices, answers
+// checkboxes with text on it
+// store answer as id
+// check if id is correct
+
+const quizData = [
+  {
+    question: 'How old is Sho',
+    a: '10',
+    b: '17',
+    c: '32',
+    d: '110'
+    correct: 'c'
+  }, {
+    question: 'What is the most used programming language in 2019',
+    a: 'Java',
+    b: 'C',
+    c: 'Pyton',
+    d: 'JavaScript',
+    correct: 'a'
+  }
+
+]
