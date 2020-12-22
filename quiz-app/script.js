ec7c694ae3580ca637024c1920489c0603ae5fe6
shoneriki <ikirenohs@gmail.com>
2020-12-22 12:13:57 +0900

    able to go through quiz and alert at end

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -48,6 +48,7 @@ const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
 
 let currentQuiz = 0;
 
@@ -60,6 +61,15 @@ function loadQuiz() {
   b_text.innerText = currentQuizData.b;
   c_text.innerText = currentQuizData.c;
   d_text.innerText = currentQuizData.d;
-
-  currentQuestion++;
 }
+
+submitBtn.addEventListener("click", () => {
+  currentQuiz++;
+
+  if(currentQuiz < quizData.length) {
+    loadQuiz();
+  } else {
+    alert('You finished! ')
+  }
+
+})
