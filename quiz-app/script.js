d13d39e57e9cb828df5af50b92f71ffbf77db36c
shoneriki <ikirenohs@gmail.com>
2020-12-22 11:26:50 +0900

    base app for future use

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -5,19 +5,40 @@
 
 const quizData = [
   {
-    question: 'How old is Sho',
-    a: '10',
-    b: '17',
+    question: 'How old is Sho as of 2020',
+    a: '28',
+    b: '30',
     c: '32',
-    d: '110'
+    d: '35'
     correct: 'c'
   }, {
-    question: 'What is the most used programming language in 2019',
+    question: 'What is the most used programming language in 2020',
     a: 'Java',
     b: 'C',
-    c: 'Pyton',
+    c: 'Python',
     d: 'JavaScript',
-    correct: 'a'
+    correct: 'c'
+  }, {
+    question: 'Who was the President of the US in 2020',
+    a: 'George Soros',
+    b: 'Donald Trump',
+    c: 'Joe Biden',
+    d: 'Michelle Obama',
+    correct: 'b'
+  }, {
+    question: 'What does HTML stand for',
+    a: 'Hypertext Markup Language',
+    b: 'Helicopter Text Making Language',
+    c: 'Helicopters Terminals Motorboats Lamborginis',
+    d: 'Hacking Text Markup Language',
+    correct: a
+  }, {
+    question:'What year was JavaScript launched?',
+    a: '1996',
+    b: '1995',
+    c: '1994',
+    d: 'none of the above',
+    correct: d
   }
 
 ]
