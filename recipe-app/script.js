3dd72ecbf34677c6ec6c69d3c3a5811d95c3a6e0
shoneriki <ikirenohs@gmail.com>
2020-12-23 22:28:52 +0900

    styled meal-info and moved it around

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -1,6 +1,7 @@
 const mealsEl = document.getElementById('meals');
 const favoriteContainer = document.getElementById('fav-meals')
 const mealPopup = document.getElementById('meal-popup');
+const mealInfo = document.getElementById("meal-info");
 const popupCloseBtn = document.getElementById('close-popup');
 
 const searchTerm = document.getElementById('search-term');
