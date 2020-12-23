9980603abf061e83a62d6cc54d9dc780171f5e0a
shoneriki <ikirenohs@gmail.com>
2020-12-23 17:54:20 +0900

    getRandomMeal works with console

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -1,13 +1,26 @@
+const meals = document.getElementById('meals');
+
 // random meal
+getRandomMeal();
+
 async function getRandomMeal() {
-  const randomMeal =  fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+  const resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+
+  const respData = await resp.json();
+  const randomMeal = respData.meals[0];
+
+  addMeal(randomMeal, true);
 }
 
 // function for meal based on id
 async function getMealById(id) {
-  const meal = fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+  const meal = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
 }
 // meals based on search
 async function getMealsBySearch(term) {
-  const meals = fetch("https://www.themealdb.com/api/json/v1/1/search.php?s="+ term);
+  const meals = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s="+ term);
+}
+
+addMeal(mealData, random = false) {
+  
 }
