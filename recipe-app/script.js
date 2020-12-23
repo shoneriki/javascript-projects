264801c0894991deab6bab6ae2a1e95cc0d330be
shoneriki <ikirenohs@gmail.com>
2020-12-23 17:43:18 +0900

    basic javascript

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -1,3 +1,13 @@
 // random meal
-// meals based on search
+async function getRandomMeal() {
+  const randomMeal =  fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+}
+
 // function for meal based on id
+async function getMealById(id) {
+  const meal = fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+}
+// meals based on search
+async function getMealsBySearch(term) {
+  const meals = fetch("https://www.themealdb.com/api/json/v1/1/search.php?s="+ term);
+}
