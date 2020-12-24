fffb4f9416fc78fde500cd69f031ce04e9613646
shoneriki <ikirenohs@gmail.com>
2020-12-24 19:24:28 +0900

    initial addNewNote function

--- notes-app/script.js
+++ notes-app/script.js
@@ -1,3 +1,19 @@
+const addBtn = document.getElementById('add');
+
+addBtn.addEventListener('click', () => {
+  addNewNote();
+});
+
+function addNewNote() {
+  const note = document.createElement('div');
+  note.classList.add('note');
+
+  note.innerHTML = `
+
+  `
+
+  document.body.appendChild('note');
+}
 const notesEl = document.querySelector('.notes');
 const editBtn = document.querySelector('.edit');
 const deleteBtn = document.querySelector('.delete');
