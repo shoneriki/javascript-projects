5ef9795f9d07968baf1afa577a2cc37e0ae7f88d
shoneriki <ikirenohs@gmail.com>
2020-12-24 13:27:51 +0900

    main div set to hidden and initial style for latter

--- notes-app/script.js
+++ notes-app/script.js
@@ -1,1 +1,11 @@
+const notesEl = document.querySelector('.notes');
+const editBtn = document.querySelector('.edit');
+const deleteBtn = document.querySelector('.delete');
 
+const main = notesEl.querySelector('.main');
+const textArea = notesEl.querySelector('textarea');
+
+editBtn.addEventListener('click', () => {
+  main.classList.toggle('hidden');
+  textArea.classList.toggle('hidden');
+})
