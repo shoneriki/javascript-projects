f0526ce169bfd93a62d3641746fb06f3f724b1b1
shoneriki <ikirenohs@gmail.com>
2020-12-24 22:38:03 +0900

    initial updateLS code

--- todo app/script.js
+++ todo app/script.js
@@ -5,17 +5,21 @@ const todos = document.getElementById("todos");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  addTodo();
+});
+
+function addTodo() {
   const todoText = input.value;
 
   if(todoText) {
     const todoEl = document.createElement("li");
     todoEl.innerText = todoText;
 
-    todoEl.addEventListener('click', () => {
-      todoEl.classList.toggle('completed');
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
     });
 
-    todoEl.addEventListener('contextmenu', (e) => {
+    todoEl.addEventListener("contextmenu", (e) => {
       e.preventDefault();
 
       todoEl.remove();
@@ -24,5 +28,22 @@ form.addEventListener("submit", (e) => {
     todos.appendChild(todoEl);
 
     input.value = '';
+
+    updateLS();
   }
-});
+}
+
+function updateLS() {
+  const todosEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    })
+  });
+
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
