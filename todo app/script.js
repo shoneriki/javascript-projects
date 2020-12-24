aa284246ecef8d0398e8430cbdde0c15d7cc64d5
shoneriki <ikirenohs@gmail.com>
2020-12-24 20:43:16 +0900

    more css on form

--- todo app/script.js
+++ todo app/script.js
@@ -1,1 +1,12 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
 
+form.addEventListener('', (e) => {
+  e.preventDefault();
+
+  const todo = input.value;
+
+  if(!todo) {
+    
+  }
+});
