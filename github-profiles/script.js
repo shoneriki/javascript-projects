c7a818bd3b6730f1f9b7f77b1d477a0f60a79709
shoneriki <ikirenohs@gmail.com>
2020-12-25 13:39:15 +0900

    createUserCard initial code

--- github-profiles/script.js
+++ github-profiles/script.js
@@ -1,4 +1,6 @@
-const APIURL = "https://api.github.com/users/"
+const APIURL = "https://api.github.com/users/";
+
+const main = document.getElementById("main");
 
 async function getUser(user) {
   const resp = await fetch(APIURL + user)
@@ -9,4 +11,23 @@ async function getUser(user) {
 
 }
 
-function
+function createUserCard(user) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  card.innerHTML = `
+    <div>
+      <img src="${user.avatar_url}" alt="${user.name}" />
+    </div>
+    <div>
+      <h2>${user.name}</h2>
+      <p>${user.bio}</p>
+
+      <ul>
+        <li>${user.followers}</li>
+        <li>${user.following}</li>
+        <li>${user.public_repos}</li>
+      </ul>
+    </div>
+  `
+}
