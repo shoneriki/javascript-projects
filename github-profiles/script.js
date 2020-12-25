90b16df42f5e3b9095bf6327f4e3e1277987e47e
shoneriki <ikirenohs@gmail.com>
2020-12-25 13:22:23 +0900

    initial JavaScript

--- github-profiles/script.js
+++ github-profiles/script.js
@@ -1,1 +1,11 @@
+const APIURL = "https://api.github.com/users/"
+
+async function getUser(user) {
+  const resp = await fetch(APIURL + user)
+  const respData = await resp.json();
+
+
+  createUserCard(respData);
+
+}
 
