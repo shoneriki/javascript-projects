09a29cc196a83e8584cafa669bd993940306f090
shoneriki <ikirenohs@gmail.com>
2020-12-28 09:44:50 +0900

    initial createUserCard function

--- github-profiles/script.js
+++ github-profiles/script.js
@@ -23,5 +23,15 @@ function createUserCard(user) {
     <div>
       <img src="${user.avatar_url}" alt="${user.name}" />
     </div>
+    <div>
+      <h2>${user.name}</h2>
+      <p>${user.bio}</p>
+
+      <ul>
+        <li>${user.followers}</li>
+        <li>${user.following}</li>
+        <li>${user.public_repos}</li>
+      </ul>
+    </div>
   `
 }
