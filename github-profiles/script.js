3379ada003c84ce2c428de4b77d79e201c081323
shoneriki <ikirenohs@gmail.com>
2020-12-28 09:48:19 +0900

    createUserCard updated

--- github-profiles/script.js
+++ github-profiles/script.js
@@ -19,19 +19,24 @@ function createUserCard(user) {
   const card = document.createElement('div');
   card.classList.add('card');
 
-  card.innerHTML = `
-    <div>
-      <img src="${user.avatar_url}" alt="${user.name}" />
-    </div>
-    <div>
-      <h2>${user.name}</h2>
-      <p>${user.bio}</p>
+  const cardHTML = `
+    <div class="card">
+      <div>
+        <img src="${user.avatar_url}" alt="${user.name}" />
+      </div>
+      <div>
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
 
-      <ul>
-        <li>${user.followers}</li>
-        <li>${user.following}</li>
-        <li>${user.public_repos}</li>
-      </ul>
+        <ul>
+          <li>${user.followers}</li>
+          <li>${user.following}</li>
+          <li>${user.public_repos}</li>
+        </ul>
+      </div>
     </div>
   `
+
+  main.innerHTML = cardHTML;
+
 }
