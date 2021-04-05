3e2e9b2f2cf8eecd164c332bd2ccc7d5cc0f188d
shoneriki <ikirenohs@gmail.com>
2021-04-05 16:15:30 +0900

    functional

--- flashcard app/script.js
+++ flashcard app/script.js
@@ -3,10 +3,25 @@ const definition = document.querySelector('.definition');
 const checkButton = document.querySelector('.check');
 const nextButton = document.querySelector('.next');
 
+dogs = {
+  JRT: "Jack Russell Terrier",
+  WC: "Welsh Corgi",
+  SH: "Siberian Husky"
+}
+
+data = Object.entries(dogs)
+
+function getRandomWord() {
+  randomTerm = data[Math.floor(Math.random() * data.length)]
+  term.innerHTML = `<h3>${randomTerm[0]}</h3>`;
+  definition.innerHTML = `<h3>${randomTerm[1]}</h3>`
+
+}
+
 checkButton.addEventListener('click', function() {
   definition.style.display = 'block';
 });
 
 nextButton.addEventListener('click', function() {
-  alert("You clicked the next button");
+  location.reload();
 });
