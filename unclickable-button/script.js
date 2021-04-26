e2030d4fce220a12c9826f67d294203ebfc1906c
shoneriki <ikirenohs@gmail.com>
2021-04-26 18:27:18 +0900

    distance from button

--- unclickable-button/script.js
+++ unclickable-button/script.js
@@ -1,6 +1,19 @@
-const fleeButton = document.getElementById('fleeing-button')
+const fleeButton = document.getElementById('fleeing-button');
 
 fleeButton.addEventListener('click', () => {
   alert('Nice Try')
   window.close()
 })
+
+document.addEventListener('mousemove', (e) => {
+  const x = e.pageX
+  const y = e.pageY
+  const buttonBox = fleeButton.getBoundingClientRect()
+  const horizontalDistanceFrom = distanceFromCenter(buttonBox.x, x, buttonBox.width)
+  const verticalDistanceFrom = distanceFromCenter(buttonBox.y, y, buttonBox.height)
+  const horizontalOffset = buttonBox.width / 2 + OFFSET
+})
+
+function distanceFromCenter(boxPosition, mousePosition, boxSize) {
+  return boxPosition = mousePosition + boxSize / 2
+}
