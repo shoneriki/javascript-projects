edd9cfbfd0ee123d8caf2fc6eee38f6818bad7e4
shoneriki <ikirenohs@gmail.com>
2021-05-25 18:01:12 +0900

    speech

--- text-to-speech/script.js
+++ text-to-speech/script.js
@@ -1,1 +1,16 @@
+const playButton = document.getElementById('play-button');
+const pauseButton = document.getElementById('pause-button');
+const stopButton = document.getElementById('stop-button');
+const textInput = document.getElementById('text');
+const speedInput = document.getElementById('speed');
 
+playButton.addEventListener('click', function(event) {
+  playText(textInput.value)
+})
+
+function playText(text) {
+  const utterance = new SpeechSynthesisUtterance(text)
+  utterance.rate = speedInput.value || 1;
+  speechSynthesis.cancel();
+  speechSynthesis.speak(utterance);
+}
