8b8e8395b5c8570c19e1e56d66ef39c144f8dcdb
shoneriki <ikirenohs@gmail.com>
2021-05-26 18:03:11 +0900

    image swipe

--- eye-control/script.js
+++ eye-control/script.js
@@ -1,7 +1,45 @@
 window.saveDataAcrossSessions = true
 
-webgazer
-  .setGazeListener((data, timestamp) => {
-    console.log(data,timestamp)
+const LOOK_DELAY = 1000;
+const LEFT_CUTOFF = window.innerWidth / 4;
+const RIGHT_CUTOFF = window.innerWidth = window.innerWidth - window.innerWidth / 4;
+
+let startLookTime = Number.POSITIVE_INFINITY;
+let lookDirection = null
+let imageElement = getNewImage();
+let nextImageElement = getNewImage(true);
+
+webgazer.setGazeListener((data, timestamp) => {
+    if (data == null || lookDirection === "STOP") return
+
+    if (data.x < LEFT_CUTOFF && lookDirection !== 'LEFT') {
+      startLookTime = timestamp;
+      lookDirection = 'LEFT'
+    } else if (data.x > RIGHT_CUTOFF && lookDirection !== 'RIGHT') {
+      startLookTime = timestamp
+      lookDirection = 'RIGHT'
+    } else if (data.x >= LEFT_CUTOFF && data.x <= RIGHT_CUTOFF) {
+      startLookTime = Number.POSITIVE_INFINITY;
+      lookDirection = null;
+    }
+
+    if (startLookTime + LOOK_DELAY < timestamp) {
+      if (lookDirection === 'LEFT') {
+        imageElement.classList.add("left");
+      } else {
+        imageElement.classList.add("right");
+      }
+
+      startLookTime = Number.POSITIVE_INFINITY;
+      lookDirection = 'STOP'
+    }
   })
 .begin()
+
+function getNewImage(next = false) {
+  const img = document.createElement('img');
+  img.src="https://picsum.photos/1000?" + Math.random();
+  if (next) img.classList.add("next")
+  document.body.append(img)
+  return img
+}
