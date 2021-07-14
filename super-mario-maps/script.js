31d74ea931671ad50f0758460de9247ddf2ce6d8
shoneriki <ikirenohs@gmail.com>
2021-07-14 20:29:19 +0900

    marker editing

--- super-mario-maps/script.js
+++ super-mario-maps/script.js
@@ -1,7 +1,7 @@
 function initMap(){
     map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 34.668274375441186, lng: 135.4290281930651},
-    zoom: 18,
+    zoom: 16.5,
     mapId: 'dca967606eeb7b76',
     mapTypeControl: false,
     fullscreenControl: false,
@@ -9,14 +9,59 @@ function initMap(){
   });
 
   const markers = [
-    [
-      "Yoshi\'s House",
-      34.66718016864912,
-      135.43061604396166,
-      "yoshi_house.svg",
-      38,
-      31
-    ]
+    		[
+			"Amity Village",
+			34.6659523754284,
+			135.43205371252157,
+			'yoshi_house.svg',
+			38,
+			31,
+		],
+		[
+			'Super Nintendo World',
+      34.66782432898129,
+      135.4305516880609,
+			'pointer.svg',
+			30,
+			47.8,
+		],
+		[
+			'Snoopy\'s Backlot Cafe',
+      34.666681637217266,
+      135.43271968800667,
+			'ghosthouse.svg',
+			40,
+			48,
+		],
+		[
+		  'Hogwarts',
+      34.668778116169996,
+      135.43156059113562,
+		  'castle.svg',
+		  40,
+		  53],
+		[
+		  'Warp Pipe',
+      34.669042001641834,
+      135.43392059671956,
+		  'pipe.svg',
+		  38,
+		  42.5],
+		[
+		  'Mario Kart: Koopa\'s Challenge',
+		  34.66820462257686,
+		  135.4299751224998,
+		  'star.svg',
+		  38,
+		  38],
+		[
+			'Discovery Restaurant',
+			34.6651187529137,
+			135.43086243016162,
+			'hill_with_eyes.svg',
+			50,
+			60.7,
+		],
   ]
 
   for (let i = 0; i < markers.length; i++) {
